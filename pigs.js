bdb2bf1f6adbb040d4cafeb49f3a002ad23bcfcf
Kaden Almaraz <kalmaraz@brooksschool.org>
2026-04-10 20:59:05 -0400

    More logic and Frontend Connection

--- pigs.js
+++ pigs.js
@@ -4,10 +4,15 @@
 
 const debugMode = true
 
-const players = [new Player(element('player0RollButton'), element('player0PassButton'), element('player0HandScore'), element('player0TotalScore')),
-                new Player(element('player1RollButton'), element('player1PassButton'), element('player1HandScore'), element('player1TotalScore')),
-                new Player(element('player2RollButton'), element('player2PassButton'), element('player2HandScore'), element('player2TotalScore')), 
-                new Player(element('player3RollButton'), element('player3PassButton'), element('player3HandScore'), element('player3TotalScore')), ]
+const players = 3 // 0, 1, 2, 3
+const buttons = [['player0RollButton', 'player0PassButton'],
+                ['player1RollButton', 'player1PassButton'],
+                ['player2RollButton', 'player2PassButton'],
+                ['player3RollButton', 'player3PassButton']]
+const texts = [['player0HandScore', 'player0TotalScore'],
+                ['player1HandScore', 'player1TotalScore'],
+                ['player2HandScore', 'player2TotalScore'],
+                ['player3HandScore', 'player3TotalScore']]
 
 const states = [['Dot', 34.90, 0], ['No Dot', 65.1, 0], ['Razorback', 87.5, 5],
                 ['Trotter', 96.3, 5], ['Snouter', 99.3, 10], 
@@ -23,22 +28,90 @@ const goal = 20
 
 // Functions //
 
+function handleClick(element) {
+    if (element == 'replayButton') {
+        handleReplay()
+    } else if (element.slice(7) == 'RollButton') {
+        actionLoop()
+    } else {
+        pass(false)
+    }
+}
+
+function handleWin() {
+    print('win!')
+
+    // implement win
+
+    element('replay').classList.remove('w3-hide')
+}
+
+function handleReplay() {
+    element('replay').classList.add('w3-hide')
+
+    init()
+}
+
+function updateTemp(out, reset, textID) {
+    if (out) {
+        element(textID).innerHTML = 'PIG OUT!'
+    } else if (reset) {
+        element(textID).innerHTML = 'Score: '
+    } else {
+        element(textID).innerHTML = 'Score: ' + tempScore
+    }
+}
+
+function updateTotal(reset, totalID) {
+    if (reset) {
+        element(totalID).innerHTML = 'Total score: '
+    } else {
+        element(totalID).innerHTML = 'Total score: ' + scores[player]
+    }
+}
+
+function enableButton(buttonID) {
+    element(buttonID).disabled = false
+}
+function disableButton(buttonID) {
+    element(buttonID).disabled = true
+}
+
 // ----------------
 //     Backend
 // ----------------
 
 // Variables //
 
-let rolls = null
-
-let totalScore = 0
+let player = 0 // start at player 0
 let tempScore = 0
+
+let rolls = null
+let scores = [0, 0, 0, 0]
 
 // Functions //
 
-function gameLoop() {
+function init() {
+    resetScores()
+
+    player = -1
+    rolls = null
+
+    for (const button of buttons) {
+        disableButton(button[0])
+        disableButton(button[1])
+    }
+    for (const text of texts) {
+        updateTemp(true, false, text[0])
+        updateTotal(true, text[1])
+    }
+
+    incrementPlayer()
+}
+
+function actionLoop() {
     rolls = inputRoll()
-    print(handlePlayerScore(score(rolls[0], rolls[1])))
+    handlePlayerScore(score(rolls[0], rolls[1]))
 }
 
 function inputRoll() {
@@ -74,52 +147,54 @@ function roll() {
     }
 }
 
-function pass() {
+function pass(out) {
     print('pass!')
 
-    totalScore += tempScore
+    if (!out) {
+        scores[player] += tempScore
+        updateTotal(false, texts[player][1])
+    } 
 
-    // pass to next player
+    updateTemp(out, true, texts[player][0])
+    disableButton(buttons[player][0])
+    disableButton(buttons[player][1])
+
+    incrementPlayer()
+
+    print(player)
 }
 
 function handlePlayerScore(score) {
-    if (score == 0) return pigOut()
+    if (score == 0) return pass(true) // pig out short circuit
     
     tempScore += score
 
-    if (totalScore + tempScore >= goal) {
-        print('WIN!')
-        // implement win
-        return totalScore + tempScore
+    updateTemp(false, false, texts[player][0])
+
+    if (scores[player] + tempScore >= goal) {
+        handleWin()
+        return scores[player] + tempScore
     }
 
-    return tempScore
+    enableButton(buttons[player][1])
 }
 
-function pigOut() {
-    print('pig out!')
-    pass(0)
-    return 0
-}
+function incrementPlayer() {
+    tempScore = 0
 
-// ----------------
-//     Classes
-// ----------------
-
-class Player {
-
-    #totalScore = 0
-
-    constructor(rollButton, passButton, scoreText, totalText) {
-        // Frontend
-        this.rollButton = rollButton
-        this.passButton = passButton
-        this.scoreText = scoreText
-        this.totalText = totalText
+    if (player == players) {
+        player = 0
+    } else {
+        player++
     }
 
-    
+    enableButton(buttons[player][0])
 }
+
+function resetScores() {
+    scores = [0, 0, 0, 0]
+}
+
 
 // ----------------
 //  Util (Wrappers)
@@ -135,4 +210,4 @@ function element(id) {
     return document.getElementById(id)
 }
 
-gameLoop()
+init()
